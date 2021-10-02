e21728ab8527848aeab97fa0691a8b2baa8b8598
YihSong <64509890+YihSong@users.noreply.github.com>
2021-10-02 15:34:29 +0800

    path self spawn, random powerup spawn

---
+++ Assets/Scripts/Path.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    public GameObject itself;
+    public GameObject pathPrefab;
+    public GameObject spawnPoint;
+
+    public GameObject powerUp;
+
+    public GameObject[] powerSpawnPoint;
+
+
+    private void Start()
+    {
+        foreach (GameObject point in powerSpawnPoint)
+        {
+            int chance = Random.Range(1, 10);
+            if (chance >= 1 && chance <= 5)
+            {
+                Instantiate(powerUp, point.transform.position, Quaternion.identity);
+            }
+        } 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Rocket")
+        {
+            StartCoroutine(NewPath());
+        }
+    }
+
+    public IEnumerator NewPath()
+    {
+        Instantiate(pathPrefab, spawnPoint.transform.position, spawnPoint.transform.rotation);
+        GetComponent<BoxCollider>().enabled = false;
+        yield return new WaitForSeconds(3f);
+        Destroy(itself);
+    }
+
+    
+}
