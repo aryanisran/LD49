e9c7d250d0949537c538ebe005ec74b9f2902674
YihSong <64509890+YihSong@users.noreply.github.com>
2021-10-03 18:07:50 +0800

    path at the start and damaged anim

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -101,6 +101,7 @@ public class PlayerController : MonoBehaviour
     {
         if (canBeDamaged)
         {
+            GetComponent<Animator>().SetTrigger("damaged");
             health--;
             StartCoroutine(Invuln());
             GameController.instance.UpdateHealth(health);
